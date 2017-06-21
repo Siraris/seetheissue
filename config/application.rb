c2fa53aec566e21564c316447d2c084c48524423
Andrew Soep <siraris@gmail.com>
2017-06-21 15:39:21 +0200

    IT's been a long time... uhhh, styling updates?

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Seetheissue
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     #http://stackoverflow.com/questions/23185434/why-does-add-index-using-gin-create-a-btree-index-instead

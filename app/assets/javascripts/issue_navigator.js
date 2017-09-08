74d4bf21d30fed6255f81d4727d8447495790469
Andrew Soep <siraris@gmail.com>
2017-09-08 13:29:58 -0500

    Updates

--- app/assets/javascripts/issue_navigator.js
+++ app/assets/javascripts/issue_navigator.js
@@ -1,17 +1,18 @@
 var issues = [];
+var categories = [];
 
 $(function() {
   // Load categories and populate them
   $.ajax({
     url: "/categories"
   }).done((data, status, xhr)=>{
-    populateCategories(JSON.parse(data));
-  });
-
-  $.ajax({
-    url: "/issues"
-  }).done((data, status, xhr)=>{
-    issues = JSON.parse(data);
+      categories = JSON.parse(data);
+      $.ajax({
+        url: "/issues"
+      }).done((data, status, xhr)=>{
+        issues = JSON.parse(data);
+        populateCategories(categories);
+      });
   });
 
   $('#category-return__btn').on("click", (e)=>{
@@ -33,11 +34,28 @@ $(function() {
 
 function populateCategories(categories) {
   const categoryArray = categories.map((category, index)=> {
-    return `<div class="category" data-category-id="${category.id}">${category.name}</div>`;
+    return `<div class="category__container">
+    <div class="category" data-category-id="${category.id}">${category.name}
+      <span class="plus">+</span>
+      <span class="minus hidden">-</span>
+    </div>
+    <div class="issues hidden">${populateIssues(category.id)}</div>
+    </div>`;
   });
   $('#categories').html(categoryArray.join(""));
-  $('.category').on("click", (e)=>{
-    populateIssues($(e.currentTarget).data("category-id"));
+  $('.category__container').on("click", (e)=>{
+    const issues = $(e.currentTarget).children('.issues'),
+    plus = $(e.currentTarget).find('.plus'),
+    minus = $(e.currentTarget).find('.minus');
+    if (issues.hasClass('hidden')) {
+      issues.removeClass('hidden');
+      plus.addClass('hidden');
+      minus.removeClass('hidden');
+    } else {
+      issues.addClass('hidden');
+      plus.removeClass('hidden');
+      minus.addClass('hidden');
+    }
   });
 }
 
@@ -47,8 +65,7 @@ function populateIssues(categoryId) {
       return `<a href="/issues/${issue.id}" class="issue">${issue.name}</a>`;
     }
   });
-  $('#issues').html(issueArray.join(""));
-  $('#categories').hide();
+  return issueArray.join("");
 }
 
 function showContent(content, button) {

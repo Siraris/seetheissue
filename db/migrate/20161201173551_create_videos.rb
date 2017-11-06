55f1dc2d6235f0970c857ff7e75c04dd89a61eae
Andrew Soep <siraris@gmail.com>
2017-11-06 16:38:09 -0600

    Test

--- db/migrate/20161201173551_create_videos.rb
+++ db/migrate/20161201173551_create_videos.rb
@@ -2,8 +2,8 @@ class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
       t.string :media_id
-      t.integer :user_id
-      t.integer :issue_id
+      t.references :user, index: true, foreign_key: true
+      t.references :issue, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
--- db/migrate/20170918211112_add_unique_user_id_issue_id_index.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueUserIdIssueIdIndex < ActiveRecord::Migration
-  def change
-    add_index :videos, ["user_id", "issue_id"], :unique => true
-  end
-end
